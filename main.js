44cc6f7f90586b88ab2451f0620ba669fbf5135d
DeLara9S-ay2526 <delara.xq@obmontessori.edu.ph>
2026-04-21 11:18:53 +0800

    .

---
+++ main.js
@@ -0,0 +1,21 @@
+function numPass() {
+    let password = "";
+    do {
+        password = prompt("Enter a password longer than 4 characters", "");
+    } while (password.length < 5);
+    window.alert("Entered Password: " + password);
+}
+
+//       C
+//       S
+// N  D  S  M  D
+//       M
+//       L
+
+// V R S
+
+// N S M V
+
+// S M Q L
+
+// I V B
